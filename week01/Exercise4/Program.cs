b07de01519bba4d15c49688ce8e11752d08c2c9c
dylan-magumura <dmagumura@byupathway.edu>
2025-09-07 02:36:34 +0200

    Exercise4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -7,7 +7,7 @@ class Program
     {
         List<int> numbers = new List<int>();
 
-        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Console.WriteLine("Please enter a list of numbers, type 0 when finished." );
 
         int number = -1;
 
